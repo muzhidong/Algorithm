eaa90460e0af971f1a912ccef7a8d9dee05ee0f7
liuxiaodong <1119441533@qq.com>
2020-11-24 13:33:15 +0800

    获取数组中和为指定值的n元素索引

--- 201116/获取数组中两元素之和等于给定值的对应索引.js
+++ 201116/获取数组中两元素之和等于给定值的对应索引.js
@@ -94,7 +94,133 @@ function getIndices(nums, target) {
 
 }
 
+
+function getIndices2(nums, target) {
+
+	// 上面算法的时间复杂度为O(n^2)，空间复杂度为O(n)，而下面的算法复杂度是时间复杂度为O(n)，空间复杂度为O(1)。
+	// 解题思路源自：https://mp.weixin.qq.com/s/wYTPPFr0Hi7BerKTfDuwfg
+
+	var result = [];
+	var map = new Map();
+	// 遍历数组
+	for (var i = 0, len = nums.length; i < len; i++) {
+
+		// 根据元素值算出另一元素值
+		var diff = target - nums[i];
+
+		// 查找映射表中键值是否有为另一元素值的，有则记录两元素对应索引。
+		if (map.get(diff)) {
+			var splitArr = map.get(diff).split(',');
+			for (var j = 0, l = splitArr.length; j < l; j++) {
+				result.push([+splitArr[j], i])
+			}
+		}
+
+		// 存入映射表，键存储元素值，值存储元素索引
+		if (map.get(nums[i])) {
+			map.set(nums[i], `${map.get(nums[i])},${i}`);
+		} else {
+			map.set(nums[i], `${i}`);
+		}
+
+	}
+
+	return result;
+}
+
+// 拓展：返回数组中和为指定值的n个元素
+function getIndices3(nums, target, n) {
+
+	if (n > nums.length) return;
+
+	var result = [];
+
+	if (n > 2) {
+
+		var temp = [...nums];
+
+		for (var k = 0; k < nums.length; k++) {
+
+			temp.splice(k, 1);
+			var arr = getIndices3(temp, target - nums[k], n - 1);
+
+			// 拼凑
+			var newArr = [];
+			for (var m = 0, l = arr.length; m < l; m++) {
+				var el = [];
+				for (var x = 0; x < n - 1; x++) {
+					if (arr[m][x] >= k) {
+						el.push(arr[m][x] + 1)
+					} else {
+						el.push(arr[m][x]);
+					}
+				}
+				el.push(k);
+				newArr.push(el);
+			}
+
+			result = result.concat(newArr);
+
+			temp = [...nums];
+
+		}
+
+		//去重
+		for (var y = 0; y < result.length; y++) {
+			for (var p = y + 1; p < result.length;) {
+				var res = result[p].join(',').match(new RegExp(`[${result[y].join('|')}]+`, 'g'));
+				if (res && res.length === n) {
+					result.splice(p, 1);
+				} else {
+					p++
+				}
+			}
+		}
+
+		return result;
+
+	}
+
+	var map = new Map();
+
+	// 遍历数组
+	for (var i = 0, len = nums.length; i < len; i++) {
+
+		// 根据元素值算出另一元素值
+		var diff = target - nums[i];
+
+		// 查找映射表中键值是否有为另一元素值的，有则记录两元素对应索引。
+		if (map.get(diff)) {
+			var splitArr = map.get(diff).split(',');
+			for (var j = 0, l = splitArr.length; j < l; j++) {
+				result.push([+splitArr[j], i])
+			}
+		}
+
+		// 存入映射表，键存储元素值，值存储元素索引
+		if (map.get(nums[i])) {
+			map.set(nums[i], `${map.get(nums[i])},${i}`);
+		} else {
+			map.set(nums[i], `${i}`);
+		}
+
+	}
+
+	return result;
+
+}
+
 console.time('get-indices');
 var result = getIndices([11, 22, 13, 1, 5, 6, 7, 890, 30, 3, 3, 67, 48], 70);
 console.log(result);
 console.timeEnd('get-indices');
+
+console.time('get-indices2');
+var result = getIndices2([11, 22, 13, 1, 5, 6, 7, 890, 30, 3, 3, 67, 48], 70);
+console.log(result);
+console.timeEnd('get-indices2');
+
+console.time('get-indices3');
+var result = getIndices3([11, 22, 37, 1, 32, 6, 7, 890, 30, 2, 3, 67, 48, 1], 70, 4);
+console.log(result);
+console.timeEnd('get-indices3');
